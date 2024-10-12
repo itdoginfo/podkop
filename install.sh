d0a1a2b801d397302c821b9d385595477643c703
itdoginfo <koalav42@gmail.com>
2024-10-13 00:53:10 +0300

    Script for install

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+REPO="https://api.github.com/repos/itdoginfo/podkop/releases/latest"
+
+DOWNLOAD_DIR="/tmp/podkop"
+mkdir -p "$DOWNLOAD_DIR"
+
+wget -qO- "$REPO" | grep -o 'https://[^"]*\.ipk' | while read -r url; do
+    filename=$(basename "$url")
+    echo "Download $filename..."
+    wget -q -O "$DOWNLOAD_DIR/$filename" "$url"
+done
+
+echo "Installed..."
+opkg install $DOWNLOAD_DIR/podkop*.ipk
+opkg install $DOWNLOAD_DIR/luci-app-podkop*.ipk
+
+echo "Install sing-box for proxy, or install and configure WG/OpenVPN/AWG/etc for VPN mode"
