5fca5840dd5bdd6efd8d299532e94641c79c6740
itdoginfo <koalav42@gmail.com>
2024-10-26 17:28:04 +0300

    Update

--- install.sh
+++ install.sh
@@ -24,12 +24,60 @@ else
     [ -f /etc/config/dhcp-opkg ] && cp /etc/config/dhcp /etc/config/dhcp-old && mv /etc/config/dhcp-opkg /etc/config/dhcp
 fi
 
-echo "Installed..."
+echo "What type of VPN or proxy will be used?"
+echo "1) VLESS, Shadowsocks (A sing-box will be installed)"
+echo "2) Wireguard"
+echo "3) AmneziaWG"
+echo "4) OpenVPN"
+echo "5) OpenConnect"
+echo "6) Skip this step"
+
+while true; do
+    read -r -p '' TUNNEL
+    case $TUNNEL in
+
+    1)
+        opkg install sing-box
+        break
+        ;;
+
+    2)
+        opkg install wireguard-tools luci-proto-wireguard luci-app-wireguard
+        printf "\e[1;32mUse these instructions to configure https://itdog.info/nastrojka-klienta-wireguard-na-openwrt/\e[0m\n"
+        break
+        ;;
+
+    3)
+        echo "As long as it's not automated"
+        printf "\e[1;32mUse script from here https://github.com/Slava-Shchipunov/awg-openwrt\e[0m\n"
+        break
+        ;;
+
+    4)
+        opkg install opkg install openvpn-openssl luci-app-openvpn
+        printf "\e[1;32mUse these instructions to configure https://itdog.info/nastrojka-klienta-openvpn-na-openwrt/\e[0m\n"
+        break
+        ;;
+
+    5)
+        opkg install opkg install openconnect luci-proto-openconnect
+        printf "\e[1;32mUse these instructions to configure https://itdog.info/nastrojka-klienta-openconnect-na-openwrt/\e[0m\n"
+        break
+        ;;
+
+    6)
+        echo "Skip"
+        break
+        ;;
+
+    *)
+        echo "Choose from the following options"
+        ;;
+    esac
+done
+
+echo "Installed podkop..."
 opkg install $DOWNLOAD_DIR/podkop*.ipk
 opkg install $DOWNLOAD_DIR/luci-app-podkop*.ipk
 
 rm -f $DOWNLOAD_DIR/podkop*.ipk $DOWNLOAD_DIR/luci-app-podkop*.ipk
-
-#/etc/init.d/ucitrack restart
-
-echo "Install sing-box for proxy, or install and configure WG/OpenVPN/AWG/etc for VPN mode"
