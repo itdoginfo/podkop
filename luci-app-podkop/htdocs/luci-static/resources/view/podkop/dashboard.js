f4be831b5e01582317fbffdf5110684e1099f462
divocat <divocatt@gmail.com>
2025-10-20 22:41:07 +0300

    fix: run prettier for all js files

--- luci-app-podkop/htdocs/luci-static/resources/view/podkop/dashboard.js
+++ luci-app-podkop/htdocs/luci-static/resources/view/podkop/dashboard.js
@@ -1,22 +1,22 @@
-'use strict';
-'require baseclass';
-'require form';
-'require ui';
-'require uci';
-'require fs';
-'require view.podkop.main as main';
+"use strict";
+"require baseclass";
+"require form";
+"require ui";
+"require uci";
+"require fs";
+"require view.podkop.main as main";
 
 function createDashboardContent(section) {
-    const o = section.option(form.DummyValue, '_mount_node');
-    o.rawhtml = true;
-    o.cfgvalue = () => {
-        main.DashboardTab.initController();
-        return main.DashboardTab.render();
-    };
+  const o = section.option(form.DummyValue, "_mount_node");
+  o.rawhtml = true;
+  o.cfgvalue = () => {
+    main.DashboardTab.initController();
+    return main.DashboardTab.render();
+  };
 }
 
 const EntryPoint = {
-    createDashboardContent,
+  createDashboardContent,
 };
 
 return baseclass.extend(EntryPoint);
--- luci-app-podkop/htdocs/luci-static/resources/view/podkop/podkop.js
+++ luci-app-podkop/htdocs/luci-static/resources/view/podkop/podkop.js
@@ -1,77 +1,98 @@
-'use strict';
-'require view';
-'require form';
-'require baseclass';
-'require network';
-'require view.podkop.main as main';
+"use strict";
+"require view";
+"require form";
+"require baseclass";
+"require network";
+"require view.podkop.main as main";
 
 // Settings content
-'require view.podkop.settings as settings';
+"require view.podkop.settings as settings";
 
 // Sections content
-'require view.podkop.section as section';
+"require view.podkop.section as section";
 
 // Dashboard content
-'require view.podkop.dashboard as dashboard';
+"require view.podkop.dashboard as dashboard";
 
 // Diagnostic content
-'require view.podkop.diagnostic as diagnostic';
-
+"require view.podkop.diagnostic as diagnostic";
 
 const EntryPoint = {
   async render() {
     main.injectGlobalStyles();
 
-    const podkopMap = new form.Map('podkop', _('Podkop Settings'), _('Configuration for Podkop service'));
+    const podkopMap = new form.Map(
+      "podkop",
+      _("Podkop Settings"),
+      _("Configuration for Podkop service"),
+    );
     // Enable tab views
     podkopMap.tabbed = true;
 
     // Sections tab
-    const sectionsSection = podkopMap.section(form.TypedSection, 'section', _('Sections'));
+    const sectionsSection = podkopMap.section(
+      form.TypedSection,
+      "section",
+      _("Sections"),
+    );
     sectionsSection.anonymous = false;
     sectionsSection.addremove = true;
-    sectionsSection.template = 'cbi/simpleform';
+    sectionsSection.template = "cbi/simpleform";
 
     // Render section content
     section.createSectionContent(sectionsSection);
 
     // Settings tab
-    const settingsSection = podkopMap.section(form.TypedSection, 'settings', _('Settings'));
+    const settingsSection = podkopMap.section(
+      form.TypedSection,
+      "settings",
+      _("Settings"),
+    );
     settingsSection.anonymous = true;
     settingsSection.addremove = false;
     // Make it named [ config settings 'settings' ]
-    settingsSection.cfgsections = function () { return ['settings']; };
+    settingsSection.cfgsections = function () {
+      return ["settings"];
+    };
 
     // Render settings content
     settings.createSettingsContent(settingsSection);
 
-
     // Diagnostic tab
-    const diagnosticSection = podkopMap.section(form.TypedSection, 'diagnostic', _('Diagnostics'));
+    const diagnosticSection = podkopMap.section(
+      form.TypedSection,
+      "diagnostic",
+      _("Diagnostics"),
+    );
     diagnosticSection.anonymous = true;
     diagnosticSection.addremove = false;
-    diagnosticSection.cfgsections = function () { return ['diagnostic']; };
+    diagnosticSection.cfgsections = function () {
+      return ["diagnostic"];
+    };
 
     // Render diagnostic content
     diagnostic.createDiagnosticContent(diagnosticSection);
 
     // Dashboard tab
-    const dashboardSection = podkopMap.section(form.TypedSection, 'dashboard', _('Dashboard'));
+    const dashboardSection = podkopMap.section(
+      form.TypedSection,
+      "dashboard",
+      _("Dashboard"),
+    );
     dashboardSection.anonymous = true;
     dashboardSection.addremove = false;
-    dashboardSection.cfgsections = function () { return ['dashboard']; };
+    dashboardSection.cfgsections = function () {
+      return ["dashboard"];
+    };
 
     // Render dashboard content
     dashboard.createDashboardContent(dashboardSection);
-
-
 
     // Inject core service
     main.coreService();
 
     return podkopMap.render();
-  }
-}
-
+  },
+};
 
 return view.extend(EntryPoint);
